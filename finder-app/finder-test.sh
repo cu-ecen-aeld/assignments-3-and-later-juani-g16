04e80ef2ebb33097687520048daaf6f205ef5b36
Juan I. Giorgetti <juan.giorgetti@itk-engineering.com>
2024-11-27 17:28:54 +0100

    Assignment 3 part 2 completed.

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=`cat conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -48,12 +48,6 @@ then
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-# if [ -f "writer" ] 
-# then
-# 	make clean
-# fi
-# make
 
 for i in $( seq 1 $NUMFILES)
 do
