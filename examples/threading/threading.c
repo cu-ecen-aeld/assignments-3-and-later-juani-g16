cabbbb9f74cca5c58e15b4aa175b5259d69e397a
Juan I. Giorgetti <juan.giorgetti@itk-engineering.com>
2024-12-05 17:04:34 +0100

    First attempt

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,21 +4,20 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
-#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+#define DEBUG_LOG(msg, ...)
+// #define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+#define ERROR_LOG(msg, ...) printf("threading ERROR: " msg "\n", ##__VA_ARGS__)
 
-void* threadfunc(void* thread_param)
+void *threadfunc(void *thread_param)
 {
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    // struct thread_data* thread_func_args = (struct thread_data *) thread_param;
     return thread_param;
 }
 
-
-bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
+bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
      * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
@@ -28,6 +27,19 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+    thread_data_t tdata;
+    tdata.mutex = mutex;
+    pthread_mutex_init(tdata.mutex, NULL);
 
+    if (pthread_create(thread, NULL, &threadfunc, &tdata) != 0)
+    {
+        tdata.thread_complete_success = false;
+    }
+    pthread_mutex_lock(tdata.mutex);
+    sleep(wait_to_obtain_ms);
+    tdata.thread_complete_success = true;
+    pthread_mutex_unlock(tdata.mutex);
+    sleep(wait_to_release_ms);
+    pthread_mutex_destroy(mutex);
+    return tdata.thread_complete_success;
+}
