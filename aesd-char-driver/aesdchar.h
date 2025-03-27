37f61880ea420c941f35f46e924caa5d4487241f
Juan I. Giorgetti <juan.giorgetti@itk-engineering.com>
2025-03-27 11:23:17 +0100

    Added aesd_dev struct elements

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -28,6 +28,10 @@ struct aesd_dev
     /**
      * TODO: Add structure(s) and locks needed to complete assignment requirements
      */
+    struct aesd_circular_buffer *buffer; /* Circular buffer to store data */
+    struct aesd_buffer_entry entry;      /* Buffer entry to store data */
+    unsigned long size;                  /* amount of data stored here */
+    struct mutex lock;                   /* Mutex to protect read and write operations */
     struct cdev cdev;     /* Char device structure      */
 };
 
