365ce4c3cdca1ec2a7e1c24dcf8dbf15680073d8
Juan I. Giorgetti <juan.giorgetti@itk-engineering.com>
2025-03-28 19:21:23 +0100

    Added debug file

--- aesd-char-driver/aesd-debug.h
+++ aesd-char-driver/aesd-debug.h
@@ -1,19 +1,20 @@
-#ifndef AESD_DEBUG_H
-#define AESD_DEBUG_H
 
-#define AESD_DEBUG 1  //Remove comment on this line to enable debug
+#ifndef AESD_CHAR_DRIVER_AESD_DEBUG_H_
+#define AESD_CHAR_DRIVER_AESD_DEBUG_H_
 
-#undef PDEBUG             /* undef it, just in case */
+#define AESD_DEBUG 1 // Remove comment on this line to enable debug
+
+#undef PDEBUG /* undef it, just in case */
 #ifdef AESD_DEBUG
-#  ifdef __KERNEL__
-     /* This one if debugging is on, and kernel space */
-#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "aesdchar: " fmt, ## args)
-#  else
-     /* This one for user space */
-#    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
-#  endif
+#ifdef __KERNEL__
+/* This one if debugging is on, and kernel space */
+#define PDEBUG(fmt, args...) printk(KERN_DEBUG "aesdchar: " fmt, ##args)
 #else
-#  define PDEBUG(fmt, args...) /* not debugging: nothing */
+/* This one for user space */
+#define PDEBUG(fmt, args...) fprintf(stderr, fmt, ##args)
+#endif
+#else
+#define PDEBUG(fmt, args...) /* not debugging: nothing */
 #endif
 
-#endif // DEBUG
+#endif /* AESD_CHAR_DRIVER_AESD_DEBUG_H_ */
